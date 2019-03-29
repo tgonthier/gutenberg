a9ba67d193c1f7a953b349a470a641fd8cff881a
Andrés <andres.maneiro@automattic.com>
2019-03-29 19:11:38 +0100

    Set color when Forward/Bacward icons has focus

--- packages/block-library/src/gallery/gallery-image.js
+++ packages/block-library/src/gallery/gallery-image.js
@@ -132,6 +132,7 @@ class GalleryImage extends Component {
 						<IconButton
 							icon="controls-back"
 							onClick={ onMoveBackward }
+							className="blocks-gallery-item__move-backward"
 							label={ __( 'Move Image Backward' ) }
 						/>
 						<IconButton
@@ -143,6 +144,7 @@ class GalleryImage extends Component {
 						<IconButton
 							icon="controls-forward"
 							onClick={ onMoveForward }
+							className="blocks-gallery-item__move-forward"
 							label={ __( 'Move Image Forward' ) }
 						/>
 					</div>
