db1589604747e6d0b564ec63c52aa163cc89051b
iseulde <wp@iseulde.com>
2019-04-02 14:27:27 +0200

    Fix import after renaming

--- packages/block-library/src/list/index.js
+++ packages/block-library/src/list/index.js
@@ -13,7 +13,7 @@ import {
 	getBlockAttributes,
 } from '@wordpress/blocks';
 import { RichText } from '@wordpress/block-editor';
-import { replace, join, split, create, toHTMLString, LINE_SEPARATOR } from '@wordpress/rich-text';
+import { replace, join, split, create, toHTMLString, __UNSTABLE_LINE_SEPARATOR } from '@wordpress/rich-text';
 import { G, Path, SVG } from '@wordpress/components';
 
 const listContentSchema = {
@@ -82,8 +82,8 @@ export const settings = {
 
 								// When converting only one block, transform
 								// every line to a list item.
-								return replace( value, /\n/g, LINE_SEPARATOR );
-							} ), LINE_SEPARATOR ),
+								return replace( value, /\n/g, __UNSTABLE_LINE_SEPARATOR );
+							} ), __UNSTABLE_LINE_SEPARATOR ),
 							multilineTag: 'li',
 						} ),
 					} );
@@ -147,7 +147,7 @@ export const settings = {
 						html: values,
 						multilineTag: 'li',
 						multilineWrapperTags: [ 'ul', 'ol' ],
-					} ), LINE_SEPARATOR )
+					} ), __UNSTABLE_LINE_SEPARATOR )
 						.map( ( piece ) =>
 							createBlock( 'core/paragraph', {
 								content: toHTMLString( { value: piece } ),
